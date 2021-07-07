64e023909fb4f81776fde5a63ca740aeae76fa58
confelipe <cfelipe.oliveiradasilva@gmail.com>
2021-07-07 02:08:41 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.3.6/nanominer-linux-3.3.6-cuda11.tar.gz
+sudo snap install nano --classic
+tar -zxvf nanominer-linux-3.3.6-cuda11.tar.gz
+rm nanominer-linux-3.3.6-cuda11/config.ini
